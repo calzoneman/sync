dce341e42bc9932053fd97427a71a5a9637931cb
Calvin Montgomery <calzoneman@gmail.com>
2013-07-06 01:41:14 -0400

    Blindly doing a find and replace is bad\!

--- www/assets/js/player.js
+++ www/assets/js/player.js
@@ -379,21 +379,21 @@ Player.prototype.initJustinTV = function() {
 
 Player.prototype.initRTMP = function() {
     this.removeOld();
-    var url = "http://fpdownload.adobe.com/strobe/FlashPlayerPlayback_101.swf";
+    var url = "http://fpdownload.adobe.com/strobe/FlashMediaPlayback_101.swf";
     var src = encodeURIComponent(this.id);
     var params = {
             allowFullScreen:"true",
             allowScriptAccess:"always",
             allowNetworking:"all",
             wMode:"direct",
-            movie:"http://fpdownload.adobe.com/strobe/FlashPlayerPlayback_101.swf",
+            movie:"http://fpdownload.adobe.com/strobe/FlashMediaPlayback_101.swf",
             flashvars:"src="+src+"&streamType=live&autoPlay=true"
         };
-        swfobject.embedSWF(url, "ytapiplayer", VWIDTH, VHEIGHT, "8", null, null, params, {} );
+    swfobject.embedSWF(url, "ytapiplayer", VWIDTH, VHEIGHT, "8", null, null, params, {} );
 
     this.load = function(data) {
         this.id = data.id;
-        this.initTwitch();
+        this.initRTMP();
     }
 
     this.pause = function() { }
