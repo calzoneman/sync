40f3e875a8cf7c73a39bb178227c004f427d249c
calzoneman <calzoneman@gmail.com>
2013-08-13 11:18:30 -0400

    2.3.3 - run update.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var Logger = require("./logger");
 var Channel = require("./channel");
 var User = require("./user");
 
-const VERSION = "2.3.2";
+const VERSION = "2.3.3";
 
 function getIP(req) {
     var raw = req.connection.remoteAddress;
