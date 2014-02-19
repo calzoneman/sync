644437ea42871d50d9d72792cb957bd6be0c3a8a
calzoneman <calzoneman@gmail.com>
2014-02-18 22:00:56 -0600

    Directory cleanup

--- tests/fastQuit.js
+++
@@ -1,9 +0,0 @@
-var io = require('socket.io-client');
-
-var socket = io.connect('http://localhost:1337');
-
-// connect, join a room, then disconnect as quickly as possible
-socket.on('connect', function () {
-    socket.emit('joinChannel', { name: 'test' });
-    socket.disconnect();
-});
