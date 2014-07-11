c522516b8867fbc7ab291ac7064cd369af14e958
Calvin Montgomery <calzoneman@gmail.com>
2014-07-10 23:03:13 -0700

    Implement serverside Google+ retrieval

--- gptest.js
+++ gptest.js
@@ -2,7 +2,7 @@ var gi = require('./lib/get-info');
 var gp = gi.Getters.gp;
 
 var link = process.argv[2];
-var id = link.replace(/https:\/\/plus\.google\.com\/photos\/(\d+)\/albums\/(\d+)/, "$1_$2");
+var id = link.replace(/https:\/\/plus\.google\.com\/photos\/(\d+)\/albums\/(\d+)\/(\d+)/, "$1_$2_$3");
 
 gp(id, function (err, data) {
     if (err) console.error(err)
