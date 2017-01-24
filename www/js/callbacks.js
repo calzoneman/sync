d7c3edfac5bf9dde9160bda19baa987f363a0bcc
Calvin Montgomery <calzoneman@gmail.com>
2017-01-23 21:56:11 -0800

    Fix for video resizing

--- www/js/callbacks.js
+++ www/js/callbacks.js
@@ -837,7 +837,7 @@ Callbacks = {
             $("#voteskip").attr("disabled", false);
         }
 
-        $("#currenttitle").text("Currently Playing: " + data.title);
+        $("#currenttitle-text").text("Currently Playing: " + data.title);
     },
 
     mediaUpdate: function(data) {
