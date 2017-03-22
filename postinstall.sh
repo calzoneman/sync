5e537fa8db2833174e4a83361557ec751327fcc2
Calvin Montgomery <calzoneman@gmail.com>
2017-03-21 20:27:29 -0700

    Instead, add env var for overriding user input

--- postinstall.sh
+++ postinstall.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+if test "$BUILD_OVERRIDE" = "Y"; then
+    echo "Running $npm_package_scripts_build_server"
+    $npm_package_scripts_build_server
+    exit $?
+fi
+
 echo "In order to run the server, the source files in src/ must be transpiled to lib/.  This will overwrite any changes you have made to the files in lib/."
 echo -n "Do you want to build now? [y/N]? "
 read answer
