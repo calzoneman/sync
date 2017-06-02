a73812c9cb8d9857a48b6dc3d319c41281d662f9
David Siaw <davidsiaw@gmail.com>
2017-06-02 15:21:16 +0900

    oopsie daisy

--- container-install.sh
+++ container-install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 apk update
-apk build-base python git nodejs nodejs-npm mysql mysql-client curl
+apk add build-base python git nodejs nodejs-npm mysql mysql-client curl
 npm install npm@latest -g
 npm install
 
