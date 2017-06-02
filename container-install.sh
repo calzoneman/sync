1c7e1c330347a1f9aca612ae1bd556470d1f1049
David Siaw <davidsiaw@gmail.com>
2017-06-02 15:20:41 +0900

    r

--- container-install.sh
+++ container-install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 apk update
-apk build-base python git add nodejs nodejs-npm mysql mysql-client curl
+apk build-base python git nodejs nodejs-npm mysql mysql-client curl
 npm install npm@latest -g
 npm install
 
