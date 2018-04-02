6e31e0aad3f2175952b3840146c98083df9bfbd1
Adam Davis <none@none.none>
2018-04-02 15:44:39 -0400

    Adding user socket disconnect callback for anonymous check.

--- src/channel/anonymouscheck.js
+++ src/channel/anonymouscheck.js
@@ -12,6 +12,12 @@ AnonymousCheck.prototype.onUserPreJoin = function (user, data, cb) {
     opts = this.channel.modules.options;
    var anonymousBanned =  opts.get("block_anonymous_users");
    if(anonymousBanned && user.isAnonymous()) {
+        user.socket.on("disconnect", function () {
+            if (!user.is(Flags.U_IN_CHANNEL)) {
+                cb("User disconnected", ChannelModule.DENY);
+            }
+        });
+        
        user.socket.emit("errorMsg", { msg : "This channel has blocked anonymous users. Please provide a user name to join."});
         user.waitFlag(Flags.U_LOGGED_IN, function () {
             cb(null, ChannelModule.PASSTHROUGH);
