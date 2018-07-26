67b1c97d8921d894e61a5747af56913205680160
Calvin Montgomery <calzoneman@gmail.com>
2018-07-25 21:07:07 -0700

    Add io.throttle-in-rate-limit for socket event rate

--- src/util/token-bucket.js
+++ src/util/token-bucket.js
@@ -1,16 +1,27 @@
 class TokenBucket {
     constructor(capacity, refillRate) {
+        if (typeof refillRate !== 'function') {
+            const _refillRate = refillRate;
+            refillRate = () => _refillRate;
+        }
+        if (typeof capacity !== 'function') {
+            const _capacity = capacity;
+            capacity = () => _capacity;
+        }
+
         this.capacity = capacity;
         this.refillRate = refillRate;
-        this.count = capacity;
+        this.count = capacity();
         this.lastRefill = Date.now();
     }
 
     throttle() {
         const now = Date.now();
-        const delta = Math.floor((now - this.lastRefill) / 1000 * this.refillRate);
+        const delta = Math.floor(
+            (now - this.lastRefill) / 1000 * this.refillRate()
+        );
         if (delta > 0) {
-            this.count = Math.min(this.capacity, this.count + delta);
+            this.count = Math.min(this.capacity(), this.count + delta);
             this.lastRefill = now;
         }
 
