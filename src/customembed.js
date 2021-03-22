ad5b83609c440587757d7c38ad48c1a6006cb0a2
Calvin Montgomery <calzoneman@gmail.com>
2021-03-21 21:20:49 -0700

    customembed: drop <object> and <embed>

--- src/customembed.js
+++ src/customembed.js
@@ -20,77 +20,12 @@ function filter(input) {
 }
 
 function getMeta($) {
-    var tag = $("embed");
-    if (tag.length !== 0) {
-        return filterEmbed(tag[0]);
-    }
-    tag = $("object");
-    if (tag.length !== 0) {
-        return filterObject(tag[0]);
-    }
-    tag = $("iframe");
+    let tag = $("iframe");
     if (tag.length !== 0) {
         return filterIframe(tag[0]);
     }
 
-    throw new Error("Invalid embed.  Input must be an <iframe>, <object>, or " +
-                    "<embed> tag.");
-}
-
-const ALLOWED_PARAMS = /^(flashvars|bgcolor|movie)$/i;
-function filterEmbed(tag) {
-    if (tag.attribs.type && tag.attribs.type !== "application/x-shockwave-flash") {
-        throw new Error("Invalid embed. Only type 'application/x-shockwave-flash' " +
-                        "is allowed for <embed> tags.");
-    }
-
-    if (!/^https:/.test(tag.attribs.src)) {
-        throw new Error("Invalid embed. Embed source must be HTTPS, plain HTTP is not supported.");
-    }
-
-    var meta = {
-        embed: {
-            tag: "object",
-            src: tag.attribs.src,
-            params: {}
-        }
-    };
-
-    for (var key in tag.attribs) {
-        if (ALLOWED_PARAMS.test(key)) {
-            meta.embed.params[key] = tag.attribs[key];
-        }
-    }
-
-    return meta;
-}
-
-function filterObject(tag) {
-    if (tag.attribs.type && tag.attribs.type !== "application/x-shockwave-flash") {
-        throw new Error("Invalid embed. Only type 'application/x-shockwave-flash' " +
-                        "is allowed for <object> tags.");
-    }
-
-    if (!/^https:/.test(tag.attribs.data)) {
-        throw new Error("Invalid embed. Embed source must be HTTPS, plain HTTP is not supported.");
-    }
-
-    var meta = {
-        embed: {
-            tag: "object",
-            src: tag.attribs.data,
-            params: {}
-        }
-    };
-
-    tag.children.forEach(function (child) {
-        if (child.name !== "param") return;
-        if (!ALLOWED_PARAMS.test(child.attribs.name)) return;
-
-        meta.embed.params[child.attribs.name] = child.attribs.value;
-    });
-
-    return meta;
+    throw new Error("Invalid embed.  Input must be an <iframe> tag");
 }
 
 function filterIframe(tag) {
