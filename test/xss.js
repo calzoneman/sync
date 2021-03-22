0b57f528bf72b07e542a99968f46e7a015abefff
Calvin Montgomery <calzoneman@gmail.com>
2021-03-21 21:31:20 -0700

    deps: bump sanitize-html

--- test/xss.js
+++ test/xss.js
@@ -5,7 +5,13 @@ describe('XSS', () => {
     describe('sanitizeHTML', () => {
         it('behaves consistently w.r.t. special chars used in emotes', () => {
             const input    = '`^~=| _-,;:!?/."()[]{}@$*\\&#%+á\t';
-            const expected = '`^~=| _-,;:!?/."()[]{}@$*\\\\&amp;#%+á\t';
+            const expected = '`^~=| _-,;:!?/."()[]{}@$*\\&amp;#%+á\t';
+            assert.strictEqual(XSS.sanitizeHTML(input), expected);
+        });
+
+        it('disallows iframes', () => {
+            const input    = '<iframe src="https://example.com"></iframe>';
+            const expected = '';
             assert.strictEqual(XSS.sanitizeHTML(input), expected);
         });
     });
