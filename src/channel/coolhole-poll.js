3ed9517efa656d1fe8779e79a49e29b4b33f4308
streeeggs <forgotmycockli@protonmail.com>
2025-03-02 15:41:11 -0500

    fix: remove test code

--- src/channel/coolhole-poll.js
+++ src/channel/coolhole-poll.js
@@ -278,7 +278,7 @@ CoolholePollModule.prototype.handleVote = function (user, data) {
 
   if (this.poll) {
     if (
-      this.poll.countVote(Math.random() * 100, {
+      this.poll.countVote(user.realip, {
         option: data.option,
         wager: data.wager,
         user: user.getName(),
