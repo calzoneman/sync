9ba64bafac7402ea9b65228857617c47fc96eacf
Kamal Davis <kamaljdavis@gmail.com>
2025-06-07 10:56:34 -0400

    fixed check against the value of "hide_motd". cleanup of the toggle function

--- www/js/callbacks.js
+++ www/js/callbacks.js
@@ -195,7 +195,7 @@ const Callbacks = {
         CHANNEL.motd = motd;
         $("#motd").html(motd);
         $("#cs-motdtext").val(motd);
-        if (motd != "" && !localStorage.getItem("hide_motd")) {
+        if (motd != "" && localStorage.getItem("hide_motd") != "true") {
             $("#motdwrap").show();
             $("#motd").show();
             $("#togglemotd").find(".glyphicon-plus")
